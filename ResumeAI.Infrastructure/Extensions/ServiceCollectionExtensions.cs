2c4b1e697f9a4d95f8c497668fa20478813de47a
Neetika Paliwal <neetikapaliwal19982026@outlook.com>
2026-08-01 01:46:41 +0530

    added day 3 changes

---
+++ ResumeAI.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResumeAI.Infrastructure.Extensions
+{
+    internal class ServiceCollectionExtensions
+    {
+    }
+}
